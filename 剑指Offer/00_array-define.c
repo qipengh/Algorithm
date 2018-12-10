740a78f6a6b694a56921f662a6b0dbe1464c6a88
qipengh <qipengh@163.com>
2018-12-10 22:04:33 +0800

    add array-define.c

---
+++ 剑指Offer/00_array-define.c
@@ -0,0 +1,88 @@
+/**
+* 数组的定义与初始化
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#define N 4
+
+/* 一维数组的定义与初始化 */
+void define_arr1() {
+    // 定义1
+    int a[10] = {1, 2, 3, 4, 6, 7, 8, 9, 10};
+
+    // 定义2: 使用变量来定义数组长度时，数组可定义，却不能同时进行初始化赋值，需要在之后单个赋值。
+    int n = 10;
+    // int b[n] = {0};
+
+    // 定义3
+    const int i = 4;
+    int c[i];
+    //c[i] = {1, 2, 3, 4};
+    int d[N] = {1, 2, 3, 4};
+
+    // 初始化1
+    int e[] = {1, 2, 3, 4};
+
+    // 初始化2
+    int f[4] = {1, 2};
+
+    // 初始化3
+    int g[4] = {0};
+}
+
+/* 二维数组的定义与初始化 */
+void define_arr2() {
+
+    int a[12] = {1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12};
+
+    // 初始化1
+    int aa[3][4] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+
+    // 初始化2
+    int bb[3][4] =  {1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12};
+
+    // 初始化3
+    int cc[][4] =  {1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12};
+
+    // 初始化4
+    int dd[][4] = {{1}, {5, 6}, {0, 0, 11}};
+
+    // 初始化5
+    int ee[3][4] =  {0};
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            ee[i][j] = 4 * i + j + 1;
+}
+
+/* 函数返回二维数组 */
+int **foo(int rows, int columns) {
+    // 定义二维数组，并申请内存
+    int **result = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        result[i] = (int *)malloc(columns * sizeof(int));
+    }
+
+    // 初始化
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            result[i][j] = 4 * i + j + 1;
+
+    return result;
+}
+
+int main(int argc, char const *argv[]) {
+    int **arr2;
+    int rows, columns;
+    rows = 3;
+    columns = 4;
+
+    arr2 = foo(rows, columns);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            printf("%3d", arr2[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
--- 剑指Offer/01_two-dimensional-array_loopup.c
+++ 剑指Offer/01_two-dimensional-array_loopup.c
@@ -1,5 +1,5 @@
 /*
-*
+* 剑指Offer 第一题： 数组、二维数组、查找
 * 题目： 在一个二维数组中(每个一维数组的长度相同)，每行都按照从左到右递增的顺序排序，
 * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
 * 判断数组中是否有该整数。
@@ -9,10 +9,11 @@
 
 // 解法1：对于从左下到右上的对角线上的所有元素来说，该元素上面的元素依次递减，右面的元素依次递增。
 // 因此可从左下角开始遍历地比较元素(避免进入交叉路口)：若大于目标值，向上走；若小于目标值，向右走。
-int Find(int target, int[][] array) {
-    int len = array.length - 1;
+// 参数：目标值，二维数组， 行长度， 列长度
+int Find(int target, int[][] array, int rows_length, int columns_length) {
+    int len = rows_length - 1;
     int i = 0;
-    while ((len >= 0) && (i < array[0].length)) {
+    while ((len >= 0) && (i < columns_length)) {
         if (array[len][i] > target) {
             len--;
         }else if (array[len][i] < target) {
@@ -24,7 +25,15 @@ int Find(int target, int[][] array) {
     return false;
 }
 
+
+
+
+
+
 int main(int argc, char const *argv[]) {
-    /* code */
+    int arr[][4] = {};
+    int rows_length = sizeof(arr) / sizeof(arr[0]);
+    int columns_length = sizeof(arr[0]) / sizeof(int)
+
     return 0;
 }
