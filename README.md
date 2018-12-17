74043578c1a9f587d1198acf1dea627e06ec9c5b
qipengh <qipengh@163.com>
2018-12-17 21:50:14 +0800

    perfect 03_find_repeated_num

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Algorithm
+### Algorithm
+# From: 剑指Offer and 大话数据结构
--- 剑指Offer/03_find_repeatable_number_of_array.c
+++ 剑指Offer/03_find_repeatable_number_of_array.c
@@ -6,6 +6,11 @@
 
 #include <stdio.h>
 
+typedef enum{
+    true,
+    false
+} bool;
+
 void PrintArray(int data[], int length) {
     for (int i = 0; i < length; i++) {
         printf("%d", data[i]);
@@ -30,7 +35,17 @@ void InsertSort(int data[], int length) {
 }
 
 /* 先将数组排序， 再遍历。时间复杂度：O(nlogn);空间复杂度：O(1)*/
-int find_with_sort(int data[], int length) {
+bool find_with_sort(int data[], int length) {
+    // 鲁棒性代码：错误处理，特殊输入
+    if (data == NULL || length <= 0) {
+        return false;
+    }
+    // 高质量代码： 边界条件
+    for (int i = 0; i < length; i++) {
+        if (data[i] < 0 || data[i] > length - 1)
+            return false;
+    }
+
     InsertSort(data, length);
     PrintArray(data, length);
 
@@ -42,6 +57,8 @@ int find_with_sort(int data[], int length) {
             tmp = data[i];
     }
     printf("\n");
+
+    return true;
 }
 
 int find_max_of_array(int data[], int length) {
@@ -53,7 +70,17 @@ int find_max_of_array(int data[], int length) {
     return max;
 }
 /* 创建哈希表找到重复元素。时间复杂度：O(n),空间复杂度：O(n) */
-void find_with_hash(int data[], int length) {
+bool find_with_hash(int data[], int length) {
+    // 鲁棒性代码：错误处理，特殊输入
+    if (data == NULL || length <= 0) {
+        return false;
+    }
+    // 高质量代码： 边界条件
+    for (int i = 0; i < length; i++) {
+        if (data[i] < 0 || data[i] > length - 1)
+            return false;
+    }
+    PrintArray(data, length);
     const int hash_length = length;
     int hashMap[hash_length];
     for (int i = 0; i < length; i++) {
@@ -63,17 +90,59 @@ void find_with_hash(int data[], int length) {
             printf("%3d", data[i]);
     }
     printf("\n");
+
+    return true;
+}
+
+/* 重排数组：从头到尾依次扫描整个数组的每个数字，比较是否和下标相等 */
+// 总的时间复杂度是O(n),空间复杂度是O(1)
+bool find_with_resort(int data[], int length, int* duplication) {
+    // 鲁棒性代码：错误处理，特殊输入
+    if (data == NULL || length <= 0) {
+        return false;
+    }
+    // 高质量代码： 边界条件
+    for (int i = 0; i < length; i++) {
+        if (data[i] < 0 || data[i] > length - 1)
+            return false;
+    }
+    PrintArray(data, length);
+    for (int i = 0; i < length; i++) {
+        while (data[i] != i) {
+            PrintArray(data, length);
+            if (data[i] == data[data[i]]) {
+                *duplication = data[i];
+                return true;
+            }
+            // swap data[i] and data[data[i]]
+            int temp = data[i];
+            data[i] = data[temp];
+            data[temp] = temp;
+        }
+    }
+    return true;
 }
 
 int main(int argc, char const *argv[]) {
-    int data[] = {2, 3, 1, 0, 2, 5, 3};
-    int length = sizeof(data) / sizeof(int);
+    // 测试用例1:长度为n的数组里包含一个或多个重复的数字
+    int data1[] = {2, 3, 1, 0, 2, 5, 3};
+    // 测试用例2：数组中不包含重复的数字
+    int data2[] = {2, 3, 1, 0, 4, 5, 6};
+    // 测试用例3：无效输入用例(输入空指针；长度为n的数组中包含0～n-1以外的数字)
+    int data3[] = {2, 3, 1, 0, 7, 9, 3};
+    int length = sizeof(data1) / sizeof(int);
 
     /* 方案一： 排序和遍历 */
-    find_with_sort(data, length);
+    printf("find_with_sort:\n");
+    bool x = find_with_sort(data3, length);
     /* 方案二： 利用哈希表查找 */
-    find_with_hash(data, length);
+    printf("find_with_hash:\n");
+    bool y = find_with_hash(data2, length);
     /* 方案三： 重排数组,比较 */
-    // find_with_compare(data, length);
+    int num;
+    printf("find_with_resort:\n");
+    if (find_with_resort(data1, length, &num) == true) {
+         printf("repeatable num:%d\n", num);
+    }
     return 0;
 }
